58f7283f5f04af4947a6764231724f1178141e50
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-05-04 11:39:27 +0100

    exercise 8 chapter 1

--- src/com/lekwacious/chapterOne/exercise/ExerciseEight.java
+++ src/com/lekwacious/chapterOne/exercise/ExerciseEight.java
@@ -3,12 +3,14 @@ package com.lekwacious.chapterOne.exercise;
 public class ExerciseEight {
    static int number = 20;
    static String name = "Eme";
-    static double accountBalance = 300000;
+   static double accountBalance = 300000;
 
-    public static void main(String[] args) {
-        ExerciseEight object1 = new ExerciseEight();
-
+    static void changeValue() {
+        ExerciseEight.number++;
+        ExerciseEight.name = "Lekwa";
+        ExerciseEight.accountBalance = 200.00;
     }
+
 
 
 }
--- src/com/lekwacious/chapterOne/exercise/ExerciseEightTest.java
+++ src/com/lekwacious/chapterOne/exercise/ExerciseEightTest.java
@@ -2,18 +2,41 @@ package com.lekwacious.chapterOne.exercise;
 
 public class ExerciseEightTest {
     public static void main(String[] args) {
+
+
+        System.out.println("Output of the static variable when called with the class name");
+        System.out.println(ExerciseEight.name);
+        System.out.println(ExerciseEight.number);
+        System.out.println(ExerciseEight.accountBalance);
+
+
         ExerciseEight object1 = new ExerciseEight();
 
         ExerciseEight object2 = new ExerciseEight();
 
-        ExerciseEight object3 = new ExerciseEight();
+        System.out.println("Output of the static variable when called with the object 1");
 
-        ExerciseEight getObject4 = new ExerciseEight();
+        System.out.println(object1.name);
+        System.out.println(object1.number);
+        System.out.println(object1.accountBalance);
 
+        System.out.println("Output of the static variable when called with the object 2");
 
-        System.out.println(object2.toString());
-        System.out.println(object3.toString());
-        System.out.println(getObject4.toString());
+        System.out.println(object2.name);
+        System.out.println(object2.number);
+        System.out.println(object2.accountBalance);
+
+        ExerciseEight.changeValue();
+
+        System.out.println("Output of the static variable of object1 after changing the value through the changeValue Method");
+        System.out.println(object1.name);
+        System.out.println(object1.number);
+        System.out.println(object1.accountBalance);
+
+        System.out.println("Output of the static variable of object2 after changing the value through the changeValue Method");
+        System.out.println(object2.name);
+        System.out.println(object2.number);
+        System.out.println(object2.accountBalance);
 
 
     }
