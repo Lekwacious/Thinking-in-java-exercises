487755a29edc217b9ba6f96842a420970d6ab8c3
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-05-04 14:33:13 +0100

    update

---
+++ src/com/lekwacious/chapterFour/exercises/ExerciseTWo.java
@@ -0,0 +1,10 @@
+package com.lekwacious.chapterFour.exercises;
+
+public class ExerciseTWo {
+    String firstName = "Eme";
+    String lastName;
+
+    public ExerciseTWo(String lastName) {
+        this.lastName = lastName;
+    }
+}
---
+++ src/com/lekwacious/chapterFour/exercises/ExerciseTwoTest.java
@@ -0,0 +1,10 @@
+package com.lekwacious.chapterFour.exercises;
+
+
+public class ExerciseTwoTest {
+    public static void main(String[] args) {
+        ExerciseTWo obj = new ExerciseTWo("Lekwa");
+
+        System.out.printf("My first name is: %s%n My lastname is : %s", obj.firstName, obj.lastName);
+    }
+}
