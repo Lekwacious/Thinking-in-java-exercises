3046f6f90aeb74f1a0cd71b6c887df208e55d24c
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-05-04 14:28:05 +0100

    chapter four

---
+++ src/com/lekwacious/chapterFour/exercises/ExerciseThree.java
@@ -0,0 +1,7 @@
+package com.lekwacious.chapterFour.exercises;
+
+public class ExerciseThree {
+    public ExerciseThree() {
+        System.out.println("This is a no argument constructor");
+    }
+}
---
+++ src/com/lekwacious/chapterFour/exercises/ExerciseThreeTest.java
@@ -0,0 +1,7 @@
+package com.lekwacious.chapterFour.exercises;
+
+public class ExerciseThreeTest {
+    public static void main(String[] args) {
+        ExerciseThree obj = new ExerciseThree();
+    }
+}
