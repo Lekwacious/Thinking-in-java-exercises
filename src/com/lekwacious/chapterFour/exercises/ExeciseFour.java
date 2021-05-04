8a6c439a07e2938329c94cc7cf76b40291a65083
Victor Ihedioha <ihedioha.victor@gmail.com>
2021-05-04 14:43:14 +0100

    update

---
+++ src/com/lekwacious/chapterFour/exercises/ExeciseFour.java
@@ -0,0 +1,11 @@
+package com.lekwacious.chapterFour.exercises;
+
+public class ExeciseFour {
+    public ExeciseFour() {
+        System.out.println("This is a no argument constructor");
+    }
+    public ExeciseFour(String message) {
+        System.out.println(message);
+    }
+
+}
---
+++ src/com/lekwacious/chapterFour/exercises/ExerciseFourTest.java
@@ -0,0 +1,9 @@
+package com.lekwacious.chapterFour.exercises;
+
+public class ExerciseFourTest {
+    public static void main(String[] args)
+    {
+        ExeciseFour obj = new ExeciseFour();
+        ExeciseFour obj2 = new ExeciseFour("There is an overloaded constructor in this class");
+    }
+}
